4fbe1b08805a38761e9296d673b17aab07264ed2
Wilian Martinez <wmartinez@q6cyber.com>
2024-01-26 19:01:24 -0600

    Add web server

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+#![deny(warnings)]
+
+use std::convert::Infallible;
+
+
+use bytes::Bytes;
+use http_body_util::Full;
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use hyper::{Request, Response};
+use hyper_util::rt::TokioIo;
+use tokio::net::TcpListener;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+
+
+// Request function
+async fn hello(_: Request<impl hyper::body::Body>) -> Result<Response<Full<Bytes>>, Infallible> {
+    Ok(Response::new(Full::new(Bytes::from("Hello World!"))))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    pretty_env_logger::init();
+
+    // Set address
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 3000);
+
+    //Bind to the port and listen for incoming TCP connections
+    let listener = TcpListener::bind(addr).await?;
+    println!("Listening on http://{}", addr);
+    loop {
+        let (tcp, _) = listener.accept().await?;
+
+        let io = TokioIo::new(tcp);
+
+        tokio::task::spawn(async move {
+            if let Err(err) = http1::Builder::new()
+                .serve_connection(io, service_fn(hello))
+                .await
+            {
+                println!("Serror serving connection: {:?}", err)
+            }
+        });
+    }
+}
